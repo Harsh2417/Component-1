e37e3eb70e6a32b0d78ae46a71a9b0ba040a7edf
Harsh2417 <65455754+Harsh2417@users.noreply.github.com>
2022-11-08 23:32:00 +0530

    Add files via upload

---
+++ src/Component1/about.js
@@ -0,0 +1,13 @@
+const About = _ => {
+
+    return (
+        <>
+        <h1>BITTO AGARWAL</h1>
+        <h4>Software Developer,Educator at Geekster</h4>
+       <a href="/">LinkedIn</a> &ensp;
+       <a href="/">GitHub</a>
+        
+        </>
+    )
+}
+export default About;
